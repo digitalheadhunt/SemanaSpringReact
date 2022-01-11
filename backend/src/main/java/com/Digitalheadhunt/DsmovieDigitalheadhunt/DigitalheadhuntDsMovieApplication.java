003b76a3c70e6df06d65cb9b30fce39354fa2fbd
digitalheadhunt <thalis_fernandes25@outlook.com>
2022-01-11 05:44:57 -0300

    feat: criando o projeto do spring React

---
+++ backend/src/main/java/com/Digitalheadhunt/DsmovieDigitalheadhunt/DigitalheadhuntDsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.Digitalheadhunt.DsmovieDigitalheadhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalheadhuntDsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalheadhuntDsMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Digitalheadhunt/DsmovieDigitalheadhunt/DigitalheadhuntDsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Digitalheadhunt.DsmovieDigitalheadhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalheadhuntDsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
